bb988f0620a500dd0a6a3ba2fee91ecf92ad1220
LukePadawalker <padurariu.luca@gmail.com>
2023-12-23 09:12:19 +0200

    added form section in html

--- js/script.js
+++ js/script.js
@@ -1,21 +1,51 @@
-console.log("JS OK");
+console.log("js ok");
 
+/*
 
-const km = prompt('inserisci i chilometri da percorrere', '5');
+ - crea una variabile km
+ - chiedi all'utente quanti chilometri vuole percorrere
+ - crea una variabile eta
+ - chiedi all'utente l'eta
+ - crea una variabile prezzochilometro (costante)
+ - crea una variabile sconto20 (costante)
+ - crea una variabile sconto40 (costante)
+ - crea una variabile prezzo-finale
+ - crea una condizione:
+     se l'eta è minore di 18 anni
+         allora calcola il prezzo finale, applica lo sconto del 20% (sconto20)
+     altrimenti se l'eta è maggiore di 65 anni
+         allora calcola il prezzo finale, applica lo sconto del 40% (sconto40)
+    altrimenti
+        calcola il prezzo finale standard 
+ - e mostra all'utente il prezzo finale
+        
+*/
+
+const km = document.getElementById("km");
 console.log(km);
 
-const eta = prompt('inserisci la tua eta', '5');
+const eta = document.getElementById("eta");
 console.log(eta);
 
-let prezzofinale = km * 0.21;
+const prezzochilometro = 0.21;
+console.log(prezzochilometro);
+
+let prezzofinale = km * prezzochilometro;
 console.log(prezzofinale);
 
+const sconto20 = (((prezzofinale) / 100) * 20);
+console.log(sconto20);
+
+const sconto40 = (((prezzofinale) / 100) * 40);
+console.log(sconto40);
+
+
 if (eta < 18){
-    prezzofinale = prezzofinale - (((prezzofinale) / 100) * 20);
+    prezzofinale = prezzofinale - sconto20;
 
 }
 else if (eta > 65){
-    prezzofinale = prezzofinale - (((prezzofinale) / 100) * 40);
+    prezzofinale = prezzofinale - sconto40;
 
 } 
 
@@ -23,6 +53,7 @@ prezzofinale = prezzofinale.toFixed(2);
 
 
 console.log(prezzofinale);
+document.getElementById("risposta").innerHTML = `Il prezzo finale è ${prezzofinale}`;
 
 
 
@@ -31,8 +62,94 @@ console.log(prezzofinale);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const km = (document.getElementById("km").value);
+// console.log(km);
+
+
+// const eta = (document.getElementById("eta").value);
+// console.log(km);
+
+
+// const prezzochilometro = 0.21;
+
+
+// let prezzo = km * prezzochilometro;
+
+
+// let prezzofinale;
+
+
+// const button = document.getElementById("calculatorbutton");
+
+// button.addEventListener("click", function() {
+//     console.log("mi hai cliccato");
+    
+    
+//     if (eta < 18){
+//         prezzofinale = prezzo - (prezzo * 0.2);
+        
+//     }
+//     else if (eta > 65){
+//         prezzofinale = prezzo - (prezzo * 0.4);
+        
+//     }      
+    
+    
+//     prezzofinale = prezzofinale.toFixed(2);
+    
+    
+    
+// });    
+
+
+// console.log(prezzofinale);
 
 
 // document.getElementById("risposta").innerHTML = `Il prezzo finale è ${prezzofinale}`;
+
+
+
+
+
+
+
+
+
+
+
 
 
